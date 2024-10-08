8962ff92ba43367b8e66b5bd6a04f69fbcc1c714
cebpereira <c.elandro.bp@gmail.com>
2024-10-08 17:10:07 -0300

    feat(database): criação do DatabaseSeeder

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,7 +3,8 @@
 namespace Database\Seeders;
 
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use App\Models\Project;
+use App\Models\Proposal;
 use Illuminate\Database\Seeder;
 
 class DatabaseSeeder extends Seeder
@@ -13,11 +14,11 @@ class DatabaseSeeder extends Seeder
      */
     public function run(): void
     {
-        // User::factory(10)->create();
-
-        User::factory()->create([
-            'name' => 'Test User',
-            'email' => 'test@example.com',
-        ]);
+        User::factory()->count(200)->create();
+        User::query()->inRandomOrder()->limit(10)->get()
+            ->each(function (User $u) {
+                $project = Project::factory()->create(['created_by' => $u->id]);
+                Proposal::factory()->count(random_int(4, 45))->create(['project_id' => $project->id]);
+            });
     }
 }
