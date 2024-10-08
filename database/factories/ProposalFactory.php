26190d35843ce3a37127e2196524a0839a5b2809
cebpereira <c.elandro.bp@gmail.com>
2024-10-08 17:09:33 -0300

    feat(proposal): criação de model, factory e migration

---
+++ database/factories/ProposalFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Database\Factories;
+
+use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Project;
+
+/**
+ * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Proposal>
+ */
+class ProposalFactory extends Factory
+{
+    /**
+     * Define the model's default state.
+     *
+     * @return array<string, mixed>
+     */
+    public function definition(): array
+    {
+        return [
+            'email' => fake()->safeEmail(),
+            'hours' => fake()->numberBetween(1, 120),
+            'project_id' => Project::factory(),
+        ];
+    }
+}
