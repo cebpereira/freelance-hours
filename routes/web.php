c86c679972af911eda4ee8fd3cee53296e626de5
cebpereira <c.elandro.bp@gmail.com>
2024-10-09 21:32:25 -0300

    feat: rotas necessárias para navegação entre telas

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
+use App\Http\Controllers\ProjectsController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+Route::get('/', [ProjectsController::class, 'index'])->name('projects.index');
+Route::get('/project/{project}', [ProjectsController::class, 'show'])->name('projects.show');
